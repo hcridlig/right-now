19cffd5de2f2457760f1e771e4b5fe6a63a5c7db
AlexD4V <alex4921@hotmail.fr>
2023-05-25 12:10:31 +0200

    reglage probleme navbar et end

--- html/nav.php
+++ html/nav.php
@@ -1,32 +1,22 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-  <head>
-    <link href="nav.css" rel="stylesheet" type="text/css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div class="container-fluid">
-      <div class="nav-item"><a href="test2.html">Right Now</a></div>
-      <div class="search-bar">
-        <input type="text" placeholder="Recherchez un restaurant">
-        <button type="submit">Rechercher</button>
-      </div>
+<head>
+  <link href="nav.css" rel="stylesheet" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="test2.html">Right Now</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Recherchez un restaurant" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Rechercher</button>
+      </form>
       <div>
-        <button class="login-btn" type="button" onclick="location.href='login.php';">Login</button>
-        <button class="signup-btn" type="button" onclick="location.href='signup.php';">Sign up</button>
-
-        <div id="mySidenav" class="sidenav">
-        <a id="closeBtn" href="#" class="close">×</a>
-          <ul>
-            <li><a href="#">A propos</a></li>
-            <li><a href="#">Historique</a></li>
-            <li><a href="#">Panier</a></li>
-            <li><a href="#"></a></li>
-          </ul>
-        </div>
-
+        <button class="btn btn-primary" type="button" onclick="location.href='login.php';">Login</button>
+        <button class="btn btn-primary" type="button" onclick="location.href='signup.php';">Sign up</button>
         <a href="#" id="openBtn">
           <span class="burger-icon">
             <span></span>
@@ -34,28 +24,37 @@
             <span></span>
           </span>
         </a>
-
       </div>
-      </div>
-    </nav>
-  
+    </div>
+  </nav>
 
+  <div id="mySidenav" class="sidenav">
+    <a id="closeBtn" href="#" class="close">×</a>
+    <ul>
+      <li><a href="#">A propos</a></li>
+      <li><a href="#">Historique</a></li>
+      <li><a href="#">Panier</a></li>
+      <li><a href="#"></a></li>
+    </ul>
+  </div>
 
-    <script type="text/javascript">
-      var sidenav = document.getElementById("mySidenav");
-      var openBtn = document.getElementById("openBtn");
-      var closeBtn = document.getElementById("closeBtn");
+  <script type="text/javascript">
+    var sidenav = document.getElementById("mySidenav");
+    var openBtn = document.getElementById("openBtn");
+    var closeBtn = document.getElementById("closeBtn");
 
-      openBtn.onclick = openNav;
-      closeBtn.onclick = closeNav;
+    openBtn.onclick = openNav;
+    closeBtn.onclick = closeNav;
 
-      /* Set the width of the side navigation to 250px */
-      function openNav() {
-        sidenav.classList.add("active");
-      }
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+      sidenav.classList.add("active");
+    }
 
-      /* Set the width of the side navigation to 0 */
-      function closeNav() {
-        sidenav.classList.remove("active");
-      }
-    </script>
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+      sidenav.classList.remove("active");
+    }
+  </script>
+</body>
+</html>
--- html/panier.php
+++ html/panier.php
@@ -1,11 +1,11 @@
   <!DOCTYPE html>
-<html style="height: 100%;">
+<html>
 <head>
-	<link href="test000.css" rel="stylesheet" type="text/css">
+	<link href="panier.css" rel="stylesheet" type="text/css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
-<body style="min-height: 100%; position: relative;">
+<body>
 
   <?php require "nav.php" ?>
 

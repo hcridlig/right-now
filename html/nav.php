85593015c3cc7160b5a903f05b277e4a4eed5188
AlexD4V <alex4921@hotmail.fr>
2023-05-30 11:29:23 +0200

    3eme essaie

--- html/nav.php
+++ html/nav.php
@@ -86,7 +86,7 @@
       <li><a href="#"></a></li>
       <li><a href="#"></a></li>
       <li><a href="#"></a></li>
-      <li style="padding-left: 3em;"><button style="background-color: red; color: white; border-radius: 10px;">Déconnexion</button></li>
+      <li style="padding-left: 3.5em; padding-top: 10em;"><button style="background-color: red; color: white; border-radius: 10px;">Déconnexion</button></li>
     </ul>
   </div>
 

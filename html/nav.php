bbe76f605d8b5f4878b16974143abced456fae33
AlexD4V <alex4921@hotmail.fr>
2023-05-30 10:40:56 +0200

    petit commit mon compte dans navbar

--- html/nav.php
+++ html/nav.php
@@ -69,7 +69,7 @@
   <div id="mySidenav" class="sidenav">
     <a id="closeBtn" href="#" class="close">X</a>
     <ul>
-      <li><a href="#">A propos</a></li>
+      <li><a href="#">Mon compte</a></li>
       <li><a href="recap.php">Historique</a></li>
       <li><a href="panier.php">Panier</a></li>
       <li><a href="#"></a></li>

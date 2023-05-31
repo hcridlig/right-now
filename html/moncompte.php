fb624195f207225e189f8fd8d7c10e625825c19a
Hugo <hugo.cridlig@esme.fr>
2023-06-01 00:00:55 +0200

    bouton modifier

--- html/moncompte.php
+++ html/moncompte.php
@@ -1,3 +1,6 @@
+<?php session_start(); 
+require 'Scripts/session.php';?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,58 +22,41 @@
 
     <?php require_once 'nav.php'; ?>
 
+    <?php 
+    require_once 'Scripts/connection_db.php';
+    $stmt = $connection->prepare("SELECT name, email, address FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $utilisateur = $result->fetch_assoc();
+
+    ?>
+
   <div class="container" style="margin-top: 8em; margin-bottom: 8em;">
     <div class="card">
       <h5 class="card-header">Informations du compte</h5>
       <div class="card-body">
         <div class="form-group">
           <label for="nom_utilisateur">Nom:</label>
-          <input type="text" class="form-control" id="nom_utilisateur" readonly>
+          <input type="text" class="form-control" id="nom_utilisateur" readonly value="<?php echo $utilisateur["name"];?>">
         </div>
         <div class="form-group">
           <label for="email_utilisateur">Email:</label>
-          <input type="email" class="form-control" id="email_utilisateur" readonly>
-        </div>
-        <div class="form-group">
-          <label for="date_naissance_utilisateur">Date de naissance:</label>
-          <input type="text" class="form-control" id="date_naissance_utilisateur" readonly>
+          <input type="email" class="form-control" id="email_utilisateur" readonly value="<?php echo $utilisateur["email"];?>">
         </div>
         <div class="form-group">
           <label for="adresse_utilisateur">Adresse:</label>
-          <textarea class="form-control" id="adresse_utilisateur" readonly></textarea>
+          <textarea class="form-control" id="adresse_utilisateur" readonly value="<?php echo $utilisateur["address"];?>"></textarea>
         </div>
         <div class="form-group">
+          <button class="btn btn-secondary" id="btn_supprimer">Modifier</button>
           <button class="btn btn-danger" id="btn_supprimer">Supprimer le compte</button>
         </div>
       </div>
     </div>
   </div>
 
-  <script>
-    // Récupérer les informations de l'utilisateur depuis le backend
-    // Remplacez les valeurs factices ci-dessous par les vraies données de l'utilisateur
 
-    var utilisateur = {
-      nom: "John Doe",
-      email: "john.doe@example.com",
-      dateNaissance: "01/01/1990",
-      adresse: "123 Rue de l'Exemple, Ville, Pays"
-    };
-
-    // Mettre à jour les éléments HTML avec les informations de l'utilisateur
-
-    document.getElementById("nom_utilisateur").value = utilisateur.nom;
-    document.getElementById("email_utilisateur").value = utilisateur.email;
-    document.getElementById("date_naissance_utilisateur").value = utilisateur.dateNaissance;
-    document.getElementById("adresse_utilisateur").value = utilisateur.adresse;
-
-    // Gérer l'événement de clic du bouton de suppression du compte
-
-    document.getElementById("btn_supprimer").addEventListener("click", function() {
-      // Code pour supprimer le compte utilisateur ici
-      alert("Le compte a été supprimé.");
-    });
-  </script>
 
 <?php require_once 'end.php'; ?>
 
